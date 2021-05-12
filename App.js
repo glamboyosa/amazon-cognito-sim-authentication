8572d4b2b4b72b01305d04688b3fc56b4e5dffca
glamboyosa <ogbemudiatimothy@gmail.com>
2021-05-12 22:55:54 +0100

    chore: moved contents from `src` up

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import {StyleSheet, View} from 'react-native';
-import {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
 import Onboarding from 'react-native-onboarding-swiper';
 import DoneButton from './components/DoneButton';
 import NextButton from './components/NextButton';
@@ -18,18 +18,20 @@ const App = () => {
           pages={[
             {
               backgroundColor: '#FF8C00',
-              title='Blazingly Fast',
-              subtitle='Up to 50x faster than alternatives.'
+              title: 'Blazingly Fast',
+              subtitle: 'Up to 50x faster than alternatives.',
             },
             {
               backgroundColor: '#00FF7F',
               title: 'Get Started',
-              subtitle: 'Create an account to get started.'
-            }
+              subtitle: 'Create an account to get started.',
+            },
           ]}
         />
       ) : (
-        <View style={styles.container}></View>
+        <View style={styles.container}>
+          <Text style={styles.heading}>tru.ID + Amazon Cognito</Text>
+        </View>
       )}
     </>
   );
@@ -40,6 +42,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  heading: {
+    fontSize: 40,
   },
 });
 
